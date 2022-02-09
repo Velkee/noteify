5b7ad61f43044de4189acd4e065a4841cfecd93e
S1LV3R <s1lv3r@corax.team>
2022-02-09 18:50:39 +0100

    remove unused comment

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,6 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 import { Command } from '../types/command';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default class PingCommand implements Command {
   public data = new SlashCommandBuilder()
     .setName('ping')
